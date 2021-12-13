c018ce6edefcc00c8706aeac5cb2c392600a33a2
Benjamin Sipes <benjamin.sipes@ucsf.edu>
2021-12-13 15:30:16 -0800

    Added 'genericLabel' option to ANTS registration

--- nipype/interfaces/ants/registration.py
+++ nipype/interfaces/ants/registration.py
@@ -418,6 +418,7 @@ class RegistrationInputSpec(ANTSCommandInputSpec):
         "BSpline",
         "MultiLabel",
         "Gaussian",
+        "genericLabel",
         argstr="%s",
         usedefault=True,
     )
